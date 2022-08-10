2f11ef443a6bf89ebf377d278063aa9ba766e492
Kundan <kunnuranka@gmail.com>
2022-08-10 22:21:21 +0530

    added api

--- api/views.py
+++ api/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+
 from django.views.decorators.http import require_http_methods
 from .models import *
+from django.views.decorators.csrf import csrf_exempt
 import json
 from django.http import HttpResponse
 
-require_http_methods(["POST"])
+@csrf_exempt
 def postName(request):
-    print(request.POST)
-    obj = Name(name=request.POST.get("name"," "))
-    obj.save()
-    return HttpResponse(json.dumps({"name" : obj.name}), content_type="application/json")
+    if request.method == "POST":
+        data = json.loads(request.body)
+        obj = Name(name=data.get("name"," "))
+        obj.save()
+        return JsonResponse({"name" : obj.name})
